f919a3d5ce5cd9f62fb7ace507554157a93e75f7
‪Ebrahim Amgad <ebrahimamgad110@gmail.com>
2025-10-08 14:59:31 +0300

    Settings Page - Finish Social Info Widget

--- settings.js
+++ settings.js
@@ -3,7 +3,15 @@ const emailInput = document.querySelector('.email');
 document.querySelector('.change').addEventListener('click', _ => {	
 	const isDisabled = emailInput.disabled;
 	if (isDisabled) {
+		emailInput.style.cursor = "text";
+		emailInput.style.backgroundColor = "#fff";
+		emailInput.style.color = "#000";
 		emailInput.removeAttribute('disabled');
 		emailInput.focus();
-	} else emailInput.disabled = !isDisabled;
+	} else { 
+		emailInput.disabled = !isDisabled;
+		emailInput.style.cursor = "no-drop";
+		emailInput.style.backgroundColor = "#f0f4f8";
+		emailInput.style.color = "#bbb";
+	};
 });
