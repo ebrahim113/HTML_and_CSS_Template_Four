18500c8455ac6c47f0cec9ced36286230461505a
‪Ebrahim Amgad <ebrahimamgad110@gmail.com>
2025-10-26 14:16:27 +0300

    Profile Page - Finish Overview Widget

--- settings.js
+++
@@ -1,8 +0,0 @@
-const emailInput = document.querySelector('.email');
-
-document.querySelector('.change').addEventListener('click', _ => {	
-	if (emailInput.disabled) {
-		emailInput.removeAttribute('disabled');
-		emailInput.focus();
-	} else emailInput.setAttribute('disabled', '');
-});
